d833fd2518b8f895f11d51dd3c68f25349d9947c
JustARand0m <korbinian.federholzner@t-online.de>
2019-01-13 01:00:11 +0100

    Added Images to Question and Solution in Quiz

--- app/src/main/java/com/example/xxxxx/flashcards/Fullscreen.java
+++ app/src/main/java/com/example/xxxxx/flashcards/Fullscreen.java
@@ -3,6 +3,8 @@ package com.example.xxxxx.flashcards;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -47,6 +49,7 @@ public class Fullscreen extends AppCompatActivity {
     }
 
     public static void setPic(ImageView imageView, String mCurrentPhotoPath){
+
         int targetW = imageView.getWidth();
         int targetH = imageView.getHeight();
 
@@ -63,6 +66,49 @@ public class Fullscreen extends AppCompatActivity {
         bmOptions.inPurgeable = true;
 
         Bitmap bitmap = BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
+
+
+        bitmap = RotatImage(mCurrentPhotoPath, bitmap);
+
         imageView.setImageBitmap(bitmap);
     }
+
+    public static Bitmap RotatImage(String photoPath, Bitmap bitmap){
+        int orientation = 0;
+        try {
+            ExifInterface ei = new ExifInterface(photoPath);
+            orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_UNDEFINED);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Bitmap rotatedBitmap = null;
+        switch(orientation) {
+
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                rotatedBitmap = rotateImage(bitmap, 90);
+                break;
+
+            case ExifInterface.ORIENTATION_ROTATE_180:
+                rotatedBitmap = rotateImage(bitmap, 180);
+                break;
+
+            case ExifInterface.ORIENTATION_ROTATE_270:
+                rotatedBitmap = rotateImage(bitmap, 270);
+                break;
+
+            case ExifInterface.ORIENTATION_NORMAL:
+            default:
+                rotatedBitmap = bitmap;
+        }
+        return rotatedBitmap;
+    }
+
+    public static Bitmap rotateImage(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(),
+                matrix, true);
+    }
+
 }
